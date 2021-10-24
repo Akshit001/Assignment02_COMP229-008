0b6a6e4d1a08d37b91713c6d04ef8ad86765919b
Akshit001 <aknarang62@gmail.com>
2021-10-24 03:22:57 -0400

    Fivth Commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,6 @@ module.exports =
 {
     //"URI": "mongodb://localhost/phn_contacts",
     //Atlas Connection
-    "URI": "mongodb+srv://Akshit:uKMUTxvQ8Hq24Ld6@mongodbserver.fejof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Akshit:uKMUTxvQ8Hq24Ld6@mongodbserver.fejof.mongodb.net/phn_contacts?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
