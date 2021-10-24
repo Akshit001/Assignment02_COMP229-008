fb46d7e5eac62bbb5481431ad888a04a4d321659
Akshit001 <aknarang62@gmail.com>
2021-10-24 03:18:33 -0400

    Forth Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/phn_contacts",
+    //Atlas Connection
     "URI": "mongodb+srv://Akshit:uKMUTxvQ8Hq24Ld6@mongodbserver.fejof.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
